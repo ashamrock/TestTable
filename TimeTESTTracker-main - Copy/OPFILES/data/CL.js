4c972ce0a4b32f9f3fe22863018edc6de5765468
A.S <a.shamrock4@gmail.com>
2023-11-17 11:56:15 -0500

    Add files via upload

---
+++ TimeTESTTracker-main - Copy/OPFILES/data/CL.js
@@ -0,0 +1,38 @@
+var clientList =[
+[
+0, //Client Number
+0, //Invoice Number
+"Name", 
+"Street",
+"City",
+"Phone Number",
+"Attention",
+"Email",
+"Alternate Email",
+],
+
+[
+1,
+2,
+"Frank Novak and Sons, Inc.", 
+"23940 Miles Road",
+"Bedford Heights Ohio 44128",
+"(216) 475-5440",
+"Accounts Payable",
+"pozimek@franknovak.com",
+"pozimek@franknovak.com",
+],
+
+[
+2,
+0,
+"John Misny", 
+"26319 forest view dr",
+"Euclid OH 44132",
+"(216) 544-7673",
+"Attn: John Misny",
+"Email",
+"",
+],
+
+];
